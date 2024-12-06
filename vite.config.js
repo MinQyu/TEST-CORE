b5603826ab105a417e623ea7a5dae9709884533e
Min-gyu <jorushs@gmail.com>
2024-12-06 15:51:51 +0900

    포켓베이스 기반 웹 앱 배포

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,23 @@
-
 import { defineConfig } from "vite";
 import { resolve } from "node:path";
 
-
- const config = defineConfig({
-  resolve:{
-    alias:[
-      {find:"@", replacement:"/src"}
-    ]
+const config = defineConfig({
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
   },
-  build:{
-    outDir:'docs',
-    rollupOptions:{
-      input:{
-        main: resolve(__dirname,'index.html'),
-        product: resolve(__dirname,'src/pages/product/index.html'),
-        login: resolve(__dirname,'src/pages/login/index.html'),
-      }
-    }
-  }
-})
+  build: {
+    outDir: "docs",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        product: resolve(__dirname, "src/pages/product/index.html"),
+        login: resolve(__dirname, "src/pages/login/index.html"),
+        detail: resolve(__dirname, "src/pages/detail/index.html"),
+        newPost: resolve(__dirname, "src/pages/newPost/index.html"),
+        register: resolve(__dirname, "src/pages/register/index.html"),
+      },
+    },
+  },
+});
 
-export default config
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default config;
